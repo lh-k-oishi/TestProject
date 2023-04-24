d6f5cdb3676207401eb5f3782a9f1b54e5b91029
KoshiOishi <k-oishi@landho.co.jp>
2023-04-24 18:53:52 +0900

    コメント追加 EnableAppearの処理をよりスマートに

--- Assets/Script/QuarterPlayer.cs
+++ Assets/Script/QuarterPlayer.cs
@@ -6,40 +6,46 @@ using UnityEngine.InputSystem;
 
 public class QuarterPlayer : MonoBehaviour
 {
-    [SerializeField] private float fSpeed = 0.01f;                  //移動スピード
+    [SerializeField] private float fSpeed = 0.01f;                                  //移動スピード
 
-    public static int s_generatedCount { get; private set; } = 0;   //生成されたプレイヤーの数
-    public static Vector3[] s_sharedVel { get; private set; } = new Vector3[4];   //共有する移動方向
-    public static bool[] s_isUnite { get; private set; } = new bool[4];   //共有する合体状況
+    public static int s_generatedCount { get; private set; } = 0;                   //生成されたプレイヤーの数
+    public static Vector3[] s_sharedVel { get; private set; } = new Vector3[4];     //共有する移動方向
+    public static bool[] s_isUnite { get; private set; } = new bool[4];             //共有する合体状況
 
-    private int playerNum = -1;                                     //プレイヤー番号
-    private Vector3 moveStartPos = Vector3.zero;                //移動開始位置
-    private Vector3 moveEndPos = Vector3.zero;                  //移動終了位置
+    private int playerNum = -1;                     //プレイヤー番号
+    private Vector3 moveStartPos = Vector3.zero;    //移動開始位置
+    private Vector3 moveEndPos = Vector3.zero;      //移動終了位置
 
-    private Timer tmrAppear = new Timer(0, 500);                    //出現時タイマー
-    private Timer tmrUnite = new Timer(0, 500);                     //合体時タイマー
+    private Timer tmrAppear = new Timer(0, 500);    //出現時タイマー
+    private Timer tmrUnite = new Timer(0, 500);     //合体時タイマー
 
-    private InputAction move;                                       //移動インプット
-    private InputAction uniteAppear;                                      //合体インプット
+    private InputAction move;                       //移動インプット
+    private InputAction uniteAppear;                //合体インプット
 
     void Awake()
     {
+        //プレイヤー番号を割り当て
         playerNum = s_generatedCount;
         s_generatedCount++;
 
+        //インプットアクション取得
         var playerInput = GetComponent<PlayerInput>();
         move = playerInput.actions["Move"];
         uniteAppear = playerInput.actions["Unite"];
 
+        //初期状態は合体
         s_isUnite[playerNum] = true;
+        //モデル非表示
         gameObject.GetComponent<Renderer>().enabled = false;
     }
 
     void Update()
     {
+        //タイマー更新
         tmrAppear.Update();
         tmrUnite.Update();
 
+        //分身時処理
         if (s_isUnite[playerNum] == false)
         {
             //出現時更新
@@ -59,30 +65,34 @@ public class QuarterPlayer : MonoBehaviour
                 Unite();
             }
         }
+        //合体時処理
         else
         {
+            //インプット更新
             Move();
             Appear();
         }
     }
 
-    //分身しているプレイヤーが3人以下であるか
+    //分身しているプレイヤーがgeneratedCount - 1人以下であるか
     static bool EnableAppear()
     {
         int count = 0;
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < s_generatedCount; i++)
         {
             count += s_isUnite[i] == false ? 1 : 0;
         }
 
-        return count < 3;
+        return count < s_generatedCount - 1;
     }
 
-    //出現
+    //分身
     void Appear()
     {
+        //出現ボタン押下かつ人数条件満たしていたら分身させる
         if (uniteAppear.ReadValue<float>() > 0f && EnableAppear())
         {
+            //本体位置から斜め位置へ
             moveStartPos = SinglePlayer.s_sharedPos;
             if (playerNum == 0)
             {
@@ -102,7 +112,9 @@ public class QuarterPlayer : MonoBehaviour
             }
 
             tmrAppear.ReStartTimer();
+            //合体解除
             s_isUnite[playerNum] = false;
+            //モデル表示
             gameObject.GetComponent<Renderer>().enabled = true;
             //物理演算とめる
             GetComponent<Rigidbody>().Sleep();
@@ -122,9 +134,11 @@ public class QuarterPlayer : MonoBehaviour
     //移動
     private void Move()
     {
+        //ジョイスティックの方向取得
         Vector2 dir = move.ReadValue<Vector2>();
         s_sharedVel[playerNum] = new Vector3(dir.x, 0, dir.y);
 
+        //合体時は本体の位置と同期
         if (s_isUnite[playerNum])
         {
             transform.position = SinglePlayer.s_sharedPos;
@@ -138,8 +152,10 @@ public class QuarterPlayer : MonoBehaviour
     //合体
     private void Unite()
     {
+        //合体ボタン押下
         if (uniteAppear.ReadValue<float>() > 0f)
         {
+            //自己位置→本体位置
             moveStartPos = transform.position;
             moveEndPos = SinglePlayer.s_sharedPos;
             tmrUnite.StartTimer();
@@ -149,12 +165,19 @@ public class QuarterPlayer : MonoBehaviour
     //合体更新処理
     private void Uniting()
     {
+        //位置はイージングで補完
         transform.position = Easing.GetEaseValue(Easing.EasingType.InBack, moveStartPos, moveEndPos, tmrUnite);
 
+        //移動終了時
         if (tmrUnite.IsEnd())
         {
+            //モデル
             gameObject.GetComponent<Renderer>().enabled = false;
+
+            //合体
             s_isUnite[playerNum] = true;
+
+            //タイマーリセット
             tmrUnite.ResetTimer();
         }
     }
--- Assets/Script/SinglePlayer.cs
+++ Assets/Script/SinglePlayer.cs
@@ -6,9 +6,9 @@ using UnityEngine.InputSystem;
 
 public class SinglePlayer : MonoBehaviour
 {
-    [SerializeField] private float fSpeed = 0.01f;
+    [SerializeField] private float fSpeed = 0.01f;       //移動スピード
 
-    public static Vector3 s_sharedPos { get; private set; } = Vector3.zero;
+    public static Vector3 s_sharedPos { get; private set; } = Vector3.zero;     //共有される自己位置
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +25,10 @@ public class SinglePlayer : MonoBehaviour
     //移動
     private void Move()
     {
+        //4人の移動方向を加算
         for (int i = 0; i < 4; i++)
         {
+            //合体しているもののみ加算
             if (QuarterPlayer.s_isUnite[i])
             {
                 transform.position += QuarterPlayer.s_sharedVel[i] * fSpeed;
